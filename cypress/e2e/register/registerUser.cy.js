858ae3f771666a0b128629a8ca445872b8924c8b
Italo Costa <italocosta.mateus@hotmail.com>
2024-10-03 10:44:38 -0300

    suite registro de usuários completa e script de execução

--- cypress/e2e/register/registerUser.cy.js
+++ cypress/e2e/register/registerUser.cy.js
@@ -1,5 +1,12 @@
 /// <reference types="Cypress" />
 
+const inputsForm = {
+  email: () => cy.get('input[name="email"]').last(),
+  name: () => cy.get('input[name="name"]'),
+  password: () => cy.get('input[name="password"]').last(),
+  passwordConfirmation: () => cy.get('input[name=passwordConfirmation]'),
+};
+
 describe('Registro de usuário', () => {
   beforeEach(() => {
     cy.visit('https://bugbank.netlify.app/');
@@ -9,23 +16,20 @@ describe('Registro de usuário', () => {
   });
 
   it('Registro de usuário com sucesso', () => {
-    const inputsForm = {
-      email: cy.get('input[name="email"]').last(),
-      name: cy.get('input[name="name"]'),
-      password: cy.get('input[name="password"]').last(),
-      passwordConfirmation: cy.get('input[name=passwordConfirmation]'),
-    };
-
-    inputsForm.email
+    inputsForm
+      .email()
       .type('testeuser@teste.com', { force: true })
       .should('have.value', 'testeuser@teste.com');
-    inputsForm.name
+    inputsForm
+      .name()
       .type('Usuário Teste 1', { force: true })
       .should('have.value', 'Usuário Teste 1');
-    inputsForm.password
+    inputsForm
+      .password()
       .type('1234', { force: true })
       .should('have.value', '1234');
-    inputsForm.passwordConfirmation
+    inputsForm
+      .passwordConfirmation()
       .type('1234', { force: true })
       .should('have.value', '1234');
 
@@ -34,5 +38,45 @@ describe('Registro de usuário', () => {
     cy.contains('Cadastrar').click({ force: true });
 
     cy.get('#modalText').should('be.visible').contains('criada com sucesso');
+  });
+
+  it('Registro de usuário sem preenchimento de campos obrigatórios', () => {
+    inputsForm
+      .name()
+      .type('Usuário Teste 2', { force: true })
+      .should('have.value', 'Usuário Teste 2');
+
+    cy.get('#toggleAddBalance').click({ force: true });
+
+    cy.contains('Cadastrar').click({ force: true });
+
+    cy.contains('É campo obrigatório').should(
+      'have.css',
+      'visibility',
+      'visible',
+    );
+  });
+
+  it('Registro de usuário com formato de e-mail invalido', () => {
+    inputsForm
+      .email()
+      .type('testeuser.com', { force: true })
+      .should('have.value', 'testeuser.com');
+    inputsForm
+      .name()
+      .type('Usuário Teste 3', { force: true })
+      .should('have.value', 'Usuário Teste 3');
+    inputsForm
+      .password()
+      .type('12345', { force: true })
+      .should('have.value', '12345');
+    inputsForm
+      .passwordConfirmation()
+      .type('12345', { force: true })
+      .should('have.value', '12345');
+
+    cy.contains('Cadastrar').click({ force: true });
+
+    cy.contains('Formato inválido').should('have.css', 'visibility', 'visible');
   });
 });
